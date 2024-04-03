3a868f95b8420ff6bb0a6a2e8a489ddc9e5a596c
Mustafa Ahmed Raza <syed.talha@weuno.co>
2024-04-03 16:14:27 +0500

    password validation done

--- lib/src/locals_views/locals_signup/password_screen.dart
+++ lib/src/locals_views/locals_signup/password_screen.dart
@@ -3,6 +3,7 @@ import 'package:chat_app_white_label/src/components/ui_scaffold.dart';
 import 'package:chat_app_white_label/src/constants/color_constants.dart';
 import 'package:chat_app_white_label/src/constants/route_constants.dart';
 import 'package:chat_app_white_label/src/utils/navigation_util.dart';
+import 'package:chat_app_white_label/src/utils/service/validation_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -29,14 +30,17 @@ class _PasswordScreenState extends State<PasswordScreen> {
   bool _isCorrectPasswordValid = false;
   late final themeCubit = BlocProvider.of<ThemeCubit>(context);
   final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _correctPasswordController = TextEditingController();
+  final TextEditingController _correctPasswordController =
+      TextEditingController();
   final TextEditingController _countryCodeController =
       TextEditingController(text: '+92');
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
     return UIScaffold(
-        appBar: AppBarComponent(""),
+        resizeToAvoidBottomInset: false,
+        appBar: const AppBarComponent(""),
         removeSafeAreaPadding: false,
         bgColor: themeCubit.backgroundColor,
         widget: setPassword());
@@ -45,77 +49,99 @@ class _PasswordScreenState extends State<PasswordScreen> {
   Widget setPassword() {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              TextComponent(
-               StringConstants.setYourAccountPassword ,
-                style: TextStyle(
-                    fontSize: 22,
-                    color: themeCubit.textColor,
-                    fontFamily: FontConstants.fontProtestStrike),
-              ),
-              SizedBoxConstants.sizedBoxThirtyH(),
-              TextFieldComponent(
-                _passwordController,
-                title: StringConstants.password,
-                hintText: "",
-                // fieldColor: ColorConstants.lightGray.withOpacity(0.5),
-                textColor: themeCubit.textColor,
-                  onChanged: (value) {
-                    setState(() {
-                      _isPasswordValid=value.length >=8 && value.trim().isNotEmpty;
-                    });
-                  }
-              ),
-              SizedBoxConstants.sizedBoxTwentyH(),
-              TextFieldComponent(
-                _correctPasswordController,
-                title: StringConstants.confirmPassword,
-                hintText: "",
-                textColor: themeCubit.textColor,
-                  onChanged: (value) {
-                    setState(() {
-                      _isCorrectPasswordValid=value.length >=8 && value.trim().isNotEmpty;
-                    });
-                  }
-              ),
-              SizedBoxConstants.sizedBoxForthyH(),
-              TextComponent(
-                StringConstants.passwordValidation,
-                style: TextStyle(
-                  fontSize: 12,
-                  color: ColorConstants.lightGray,
+      child: Form(
+        key: _formKey,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                TextComponent(
+                  StringConstants.setYourAccountPassword,
+                  style: TextStyle(
+                      fontSize: 22,
+                      color: themeCubit.textColor,
+                      fontFamily: FontConstants.fontProtestStrike),
                 ),
-                maxLines: 4,
-              ),
+                SizedBoxConstants.sizedBoxThirtyH(),
+                TextFieldComponent(
+                  _passwordController,
+                  title: StringConstants.password,
+                  filled: true,
 
-            ],
-          ),
-          SizedBox(
-            width: MediaQuery.sizeOf(context).width * 0.9,
-            child: ButtonComponent(
-                bgcolor: _isPasswordValid && _isCorrectPasswordValid
-                    ? themeCubit.primaryColor
-                    : ColorConstants.lightGray.withOpacity(0.2),
-                textColor: _isPasswordValid && _isCorrectPasswordValid
-                    ? ColorConstants.black
-                    : ColorConstants.lightGray,
-                buttonText: StringConstants.continues,
-                onPressedFunction: () {
-                  if (widget.routeType == "OnBoarding") {
-                    NavigationUtil.push(context, RouteConstants.nameScreen);
-                  } else if (widget.routeType == "phoneNumber") {
-                    NavigationUtil.push(context, RouteConstants.signUpNumber,
-                        args: "afterEmail");
-                  }
-                }),
-          )
-        ],
+                  hintText: "",
+                  // fieldColor: ColorConstants.lightGray.withOpacity(0.5),
+                  textColor: themeCubit.textColor,
+                  onChanged: (value) {
+                    setState(() {
+                      _isPasswordValid = value.length >= 8 &&
+                          value.trim().isNotEmpty &&
+                          _formKey.currentState!.validate();
+                    });
+                  },
+
+                  validator: (password) =>
+                      ValidationService.validatePassword(password!),
+                ),
+                SizedBoxConstants.sizedBoxTwentyH(),
+                TextFieldComponent(
+                  _correctPasswordController,
+                  title: StringConstants.confirmPassword,
+                  hintText: "",
+                  filled: true,
+                  textColor: themeCubit.textColor,
+                  onChanged: (value) {
+                    setState(() {
+                      _isCorrectPasswordValid = value.length >= 8 &&
+                          value.trim().isNotEmpty &&
+                          _formKey.currentState!.validate();
+                      ;
+                    });
+                  },
+                  validator: (confirmPassword) {
+                    return ValidationService.validateConfirmPassword(
+                      confirmPass: confirmPassword!,
+                      password: _passwordController.text,
+                    );
+                  },
+                ),
+                SizedBoxConstants.sizedBoxForthyH(),
+                const TextComponent(
+                  StringConstants.passwordValidation,
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: ColorConstants.lightGray,
+                  ),
+                  maxLines: 4,
+                ),
+              ],
+            ),
+            SizedBox(
+              width: MediaQuery.sizeOf(context).width * 0.9,
+              child: ButtonComponent(
+                  bgcolor: _isPasswordValid && _isCorrectPasswordValid
+                      ? themeCubit.primaryColor
+                      : ColorConstants.lightGray.withOpacity(0.2),
+                  textColor: _isPasswordValid && _isCorrectPasswordValid
+                      ? ColorConstants.black
+                      : ColorConstants.lightGray,
+                  buttonText: StringConstants.continues,
+                  onPressedFunction: () {
+                    if (_formKey.currentState!.validate()) {
+                      if (widget.routeType == "OnBoarding") {
+                        NavigationUtil.push(context, RouteConstants.nameScreen);
+                      } else if (widget.routeType == "phoneNumber") {
+                        NavigationUtil.push(
+                            context, RouteConstants.signUpNumber,
+                            args: "afterEmail");
+                      }
+                    }
+                  }),
+            )
+          ],
+        ),
       ),
     );
   }
