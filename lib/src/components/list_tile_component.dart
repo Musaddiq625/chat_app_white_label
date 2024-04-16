44e008e4147bbce5a47f1a8b4831e5b36bd2ece5
Mustafa Ahmed Raza <syed.talha@weuno.co>
2024-04-16 19:22:35 +0500

    title added, null handling done in listtile component

--- lib/src/components/list_tile_component.dart
+++ lib/src/components/list_tile_component.dart
@@ -1,6 +1,8 @@
 import 'package:chat_app_white_label/src/components/image_component.dart';
 import 'package:chat_app_white_label/src/components/text_component.dart';
 import 'package:chat_app_white_label/src/constants/app_constants.dart';
+import 'package:chat_app_white_label/src/constants/font_constants.dart';
+import 'package:chat_app_white_label/src/constants/font_styles.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -11,14 +13,17 @@ import 'icon_component.dart';
 
 class ListTileComponent extends StatelessWidget {
   final String? leadingIcon;
-  final String iconText;
-  final String subText;
+  final String? iconText;
+  final String? subText;
+  final String? title;
   final double? subTextSize;
   final double? trailingIconSize;
   final double leadingIconWidth;
   final double leadingIconHeight;
-  final IconData subIcon;
+  final IconData? subIcon;
   final Color iconColor, subIconColor, subTextColor;
+  Color? backgroundColor;
+  Color? titleColor;
   final Function()? onTap;
   final bool isLeadingImageCircular;
   final bool isLeadingImageSVG;
@@ -26,16 +31,17 @@ class ListTileComponent extends StatelessWidget {
 
   final double? overrideLeadingIconSize;
 
-  const ListTileComponent({
+  ListTileComponent({
     super.key,
     this.leadingIcon,
-    required this.iconText,
-    required this.subText,
-    required this.onTap,
+    this.iconText,
+    this.subText,
+    this.onTap,
     this.subIcon = Icons.arrow_forward_ios,
     this.iconColor = ColorConstants.black,
     this.subIconColor = ColorConstants.lightGray,
     this.subTextColor = ColorConstants.white,
+    this.backgroundColor,
     this.subTextSize,
     this.trailingIconSize,
     this.isLeadingImageCircular = false,
@@ -44,19 +50,22 @@ class ListTileComponent extends StatelessWidget {
     this.leadingIconWidth = 15,
     this.leadingIconHeight = 15,
     this.isSocialConnected = false,
+    this.title,
+    this.titleColor,
   });
 
   @override
   Widget build(BuildContext context) {
     late final themeCubit = BlocProvider.of<ThemeCubit>(context);
+    backgroundColor ??= themeCubit.darkBackgroundColor;
+    titleColor ??= themeCubit.textColor;
     return GestureDetector(
       onTap: onTap,
       child: Container(
         width: AppConstants.responsiveWidth(context),
         decoration: BoxDecoration(
-          borderRadius: const BorderRadius.all(Radius.circular(16.0)),
-          color: themeCubit.darkBackgroundColor,
-        ),
+            borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+            color: backgroundColor),
         child: Padding(
           padding: const EdgeInsets.all(15.0),
           child: Column(
@@ -64,6 +73,16 @@ class ListTileComponent extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
+                  if (title != null)
+                    Center(
+                      child: TextComponent(
+                        title!,
+                        style: FontStylesConstants.style18(
+                          fontFamily: FontConstants.inter,
+                          color: titleColor ?? themeCubit.textColor,
+                        ),
+                      ),
+                    ),
                   if (leadingIcon != null)
                     ClipRRect(
                         borderRadius: isLeadingImageCircular
@@ -79,29 +98,34 @@ class ListTileComponent extends StatelessWidget {
                     const SizedBox(
                       width: 10,
                     ),
-                  TextComponent(
-                    iconText,
-                    style: TextStyle(fontSize: 15, color: themeCubit.textColor),
-                  ),
-                  const Spacer(),
-                  TextComponent(
-                    subText,
-                    style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                        color: subTextColor),
-                  ),
-                  SizedBox(
-                    width: isSocialConnected ? 15 : 5,
-                  ),
-                  IconComponent(
-                    iconData: subIcon,
-                    borderColor: ColorConstants.transparent,
-                    backgroundColor: ColorConstants.transparent,
-                    circleSize: trailingIconSize ?? 22,
-                    iconSize: trailingIconSize ?? 22,
-                    iconColor: subIconColor,
-                  ),
+                  if (iconText != null)
+                    TextComponent(
+                      iconText!,
+                      style:
+                          TextStyle(fontSize: 15, color: themeCubit.textColor),
+                    ),
+                  if (iconText != null) const Spacer(),
+                  if (subText != null)
+                    TextComponent(
+                      subText!,
+                      style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          color: subTextColor),
+                    ),
+                  if (subText != null)
+                    SizedBox(
+                      width: isSocialConnected ? 15 : 5,
+                    ),
+                  if (subIcon != null)
+                    IconComponent(
+                      iconData: subIcon,
+                      borderColor: ColorConstants.transparent,
+                      backgroundColor: ColorConstants.transparent,
+                      circleSize: trailingIconSize ?? 22,
+                      iconSize: trailingIconSize ?? 22,
+                      iconColor: subIconColor,
+                    ),
                 ],
               ),
             ],
